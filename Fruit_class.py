b18d3cf8c12caba2a4edd996ac4bee5953ac8914
Mehdi <mehdi.teisseire@laplateforme.io>
2025-01-31 10:06:21 +0100

    refactor code

---
+++ Fruit_class.py
@@ -0,0 +1,92 @@
+import pygame, random
+import Player_class
+
+
+class Fruit:  
+    def __init__(self, name, letter, image_path, path, effect, sound):
+        self.name = name
+        self.letter = letter
+        self.letter_path = f"assets/letters/{letter}.png"
+        self.letter_img = pygame.image.load(self.letter_path)
+        self.letter_img = pygame.transform.scale(self.letter_img, (50, 50))
+
+        self.image_path = image_path
+        self.image = pygame.image.load(image_path)
+        self.image = pygame.transform.scale(self.image, (50, 50))
+        
+        self.path = path
+        self.effect = effect
+
+        self.sound = sound
+
+        self.x = 0 #physic[0] #curb_physic(self.x, self.y)[0] #random.randint(0, 750)  
+        self.y = 600 #physic[1] #curb_physic(self.x, self.y)[1] #random.randint(0, 550)
+
+        self.parabol_width = 0
+        self.curb_center_x = 0
+        self.curb_center_y = 0
+
+        self.velocity_x = 5 #random.randint(7,10)
+
+        self.paths()
+
+        self.freeze = 0
+
+    def effects(self, active_fruits, player):
+        """Effect of each type of fruits on destroy"""
+        match self.effect:
+            case "points":
+                return self.effect_points()
+            case "freeze":
+                return self.effect_freeze(player, active_fruits)
+            case "bomb":
+                return self.effect_bomb(player)
+
+    def paths(self):
+        """Path for fruit movements"""
+        match self.path:
+            case "linear":
+                self.linear_path()
+            case "curb":
+                self.curb_path()
+            case "sin":
+                self.sin_path()
+    
+    def stop_fruit(self):
+        self.x = self.x
+        self.y = self.y
+        self.freeze -= 1
+
+    def effect_points(self):
+        return 10
+
+    def effect_freeze(self, player, active_fruits):
+        """Freeze all fruits"""
+        player.score -= 1
+        print(player.score)
+        for fruit in active_fruits:
+            fruit.freeze = 100
+        return 0
+
+            
+    def effect_bomb(self, player):
+        player.lives -= 1
+        print(player.lives)
+        return 0
+
+    def move_fruits(self):
+        """ Fruit movement (should be in curb)"""
+        self.y = self.parabol_width * ((self.x - self.curb_center_x) ** 2) + self.curb_center_y
+        self.x += self.velocity_x
+
+    def linear_path(self):
+        self.path = self.path
+
+    def curb_path(self):
+        """Moves fruits following a x^2 curb"""
+        self.parabol_width = 1 / random.randint(20, 800)
+        self.curb_center_x = random.randint(200, 600)
+        self.curb_center_y = random.randint(0, 200)
+
+    def sin_path(self):
+        self.path = self.path
--- main.py
+++ main.py
@@ -1,68 +1,11 @@
-import pygame,random,time
-import fruitsphysics
-from fruitsphysics import FruitPhysics
+import pygame
+import game
+
 # Initialize the game
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
 clock = pygame.time.Clock()
-running = True
-background = pygame.image.load('assets/background.png')
-background = pygame.transform.scale(background, (800, 600))
-last_fruit_spawn = time.time()
-SPAWN_INTERVAL = 0.7
 
-class Fruit:  
-    def __init__(self, name, image_path):
-        self.name = name
-        self.image_path = image_path
-        self.image = pygame.image.load(image_path)
-        self.image = pygame.transform.scale(self.image, (50, 50))
-        self.x = random.randint(0, 750)  
-        self.y = 600
-        self.velocity_x = random.randint(-1, 1)
-        self.velocity_y = random.randint(-22, -20)
-
-# Create fruit templates
-fruit_types = [
-    Fruit('apple', 'assets/apple.png'),
-    Fruit('banana', 'assets/banana.png'),
-    Fruit('orange', 'assets/orange.png'),
-    Fruit('watermelon', 'assets/watermelon.png'),
-    Fruit('bomb', 'assets/bomb.png'),
-    Fruit('coconut', 'assets/coconut.png'),
-
-]
-
-active_fruits = []  # List to store fruits currently on screen
-physics = FruitPhysics(active_fruits, fruit_types)
-while running:
-    current_time = time.time()
-    
-    
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    
-    # Spawn new fruit every 3 seconds
-    if current_time - last_fruit_spawn >= SPAWN_INTERVAL:
-        # Choose random fruit type and create a new instance
-        fruit_template = random.choice(fruit_types)
-        new_fruit = Fruit(fruit_template.name, fruit_template.image_path)
-        active_fruits.append(new_fruit)
-        last_fruit_spawn = current_time
-        
-    
-    # Draw everything
-    screen.blit(background, (0, 0))
-    
-    # Draw all active fruits
-    for fruit in active_fruits:
-        screen.blit(fruit.image, (fruit.x, fruit.y))
-    physics.move_fruits()
-    physics.out_of_bounds()
-    physics.destroy_fruits()
-    
-    pygame.display.flip()
-    clock.tick(60)
+game.game_start(screen, clock)
 
 pygame.quit()
