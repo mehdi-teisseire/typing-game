3a55b0f322ec9971e5853a2665d8a168dd79df7d
Mehdi <mehdi.teisseire@laplateforme.io>
2025-02-04 09:51:04 +0100

    fix: bug that makes fruits dispawn to early

--- fruitsphysics.py
+++ fruitsphysics.py
@@ -8,23 +8,13 @@ class FruitPhysics:
         
     def move_fruits(self, spawn_interval, current_interval, freezing):
         for fruit in self.active_fruits:
-            if fruit.freeze > 0:
-                if not freezing[0]:
-                    current_interval[0] = spawn_interval[0]
-                    spawn_interval[0] = 1000000
-                    freezing[0] = True
-                fruit.stop_fruit()
-            else:
-                if freezing[0]:
-                    spawn_interval[0] = current_interval[0]
-                    freezing[0] = False
-                fruit.y += fruit.velocity_y
-                fruit.velocity_y += random.randint(0, 1)
-                fruit.x += fruit.velocity_x *0.5
+            fruit.y += fruit.velocity_y
+            fruit.velocity_y += random.randint(0, 1)
+            fruit.x += fruit.velocity_x
             
     def out_of_bounds(self, player):
         for fruit in self.active_fruits[:]:
-            if fruit.y > 600 or fruit.x > 805 or fruit.x < -5:
+            if fruit.y > 600 or fruit.x > 810 or fruit.x < -5:
                 self.active_fruits.remove(fruit)
                 if fruit.name != "bomb" and fruit.name != "comet":
                     player.hearts -= 1
